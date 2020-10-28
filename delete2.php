4941319fa8b1b2b423a86775bf5470e88309ca41
baromix <42960625+baromix@users.noreply.github.com>
2020-10-28 10:17:06 +0100

    Delete delete2.php

--- delete2.php
+++
@@ -1,6 +0,0 @@
-<?php
-         $conn = new mysqli("localhost", "root", "", "library", "3308");
-         $sql = "DELETE FROM `tytuly` WHERE id_tytul=".$_POST['id_tytul']." "; 
-    mysqli_query($conn, $sql);
-    header("Location:index.php");
-?>
